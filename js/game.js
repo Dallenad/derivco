886c64a4b58ea5cf01c66eb0483752e7b69eee6c
Dymas <Dallenad@users.noreply.github.com>
2019-08-03 14:38:28 -0600

    Fixed awarding bugs

--- js/game.js
+++ js/game.js
@@ -22,7 +22,9 @@ let stripLeft,
     icons,
     positions,
     currentBalance,
-    shadow;
+    shadow,
+    prize,
+    tweens;
 let fixed = false; //Fixed Mode
 let prevValues = [0, 0, 0];
 let values = [0, 0, 0];
@@ -64,6 +66,9 @@ function create ()
     stripLeft = this.add.tileSprite(-148, 0, 141, iconHeight * 3, 'strip');
     stripCenter = this.add.tileSprite(0, 0, 141, iconHeight * 3, 'strip');
     stripRight = this.add.tileSprite(148, 0, 141, iconHeight * 3, 'strip');
+    prize = this.add.text(330, 280, '5', { fontFamily: 'Tienne', fontSize: 144, color: "rgb(255, 255, 63)" });
+    prize.setOrigin(0.5);
+    prize.setScale(0);
 
     spinButton = this.add.sprite(700, 200, 'spin');
 
@@ -115,6 +120,7 @@ function create ()
 
     icons = document.getElementsByClassName("icons");
     positions = document.getElementsByClassName("position");
+    tweens = this.tweens;
 }
 
 //Determine spin results
@@ -169,7 +175,7 @@ function spin ()
     let valueCenter = (prevValues[1] - values[1]) * iconHeight / 2;
     let valueRight = (prevValues[2] - values[2]) * iconHeight / 2;
 
-    this.tweens.add({
+    tweens.add({
         targets: stripLeft,
         tilePositionY: {
             value: "-=" + (iconHeight * duration[0] / 100 + valueLeft),
@@ -177,7 +183,7 @@ function spin ()
             ease: 'Linear'
         }
     });
-    this.tweens.add({
+    tweens.add({
         targets: stripCenter,
         tilePositionY: {
             value: "-=" + (iconHeight * duration[1] / 100 + valueCenter),
@@ -185,7 +191,7 @@ function spin ()
             ease: 'Linear'
         }
     });
-    this.tweens.add({
+    tweens.add({
         targets: stripRight,
         tilePositionY: {
             value: "-=" + (iconHeight * duration[2] / 100 + valueRight),
@@ -196,6 +202,26 @@ function spin ()
     });
 }
 
+function showPrize(amount)
+{
+    prize.setText(amount);
+    tweens.add({
+        targets: prize,
+        scaleX: { value: 1, duration: 200, ease: 'Power1' },
+        scaleY: { value: 1, duration: 200, ease: 'Power1' },
+        y: { value: "-=200", duration: 2000, ease: 'Power1' },
+        alpha: { value: 0, duration: 1000, ease: 'Power1', delay: 1000 },
+        onComplete: resetPrize
+    });
+}
+
+function resetPrize ()
+{
+    prize.alpha = 1;
+    prize.y += 200;
+    prize.setScale(0, 0);
+}
+
 function stopSpin ()
 {
     //Get the game ready for another spin
@@ -204,7 +230,7 @@ function stopSpin ()
     spinButton.setFrame(0);
     shadow.style.visibility = "hidden";
     prevValues = values.slice(0, 3);
-    //console.log(values);
+    console.log(values);
 
     //Reward the player accordingly
 
@@ -213,24 +239,34 @@ function stopSpin ()
         switch (values[0])
         {
             case 9: balance += 50; //3 3xBARs on the center
+                    showPrize(50);
                     break;
             case 8: balance += 50; //3 3xBARs at the bottom
+                    showPrize(50);
                     break;
             case 7: balance += 2000; //3 cherries at the top
+                    showPrize(2000);
                     break;
             case 6: balance += 1000; //3 cherries on the center
+                    showPrize(1000);
                     break;
             case 5: balance += 4000; //3 cherries at the bottom
+                    showPrize(4000);
                     break;
             case 4: balance += 150; //3 7s on the center
+                    showPrize(150);
                     break;
             case 3: balance += 150; //3 7s at the bottom
+                    showPrize(150);
                     break;
             case 2: balance += 20; //3 2xBARs on the center
+                    showPrize(20);
                     break;
             case 1: balance += 20; //3 2xBARs at the bottom
+                    showPrize(20);
                     break;
             case 0: balance += 10; //3 BARs on the center
+                    showPrize(10);
                     break;
         }
     }
@@ -238,21 +274,31 @@ function stopSpin ()
     {
         //Checking combination of cherries and 7s
 
-        if (
-            (values[0] % 2 == 0 && values[1] % 2 == 0 && values[2] % 2 == 0) ||
-            (Math.abs(values[0] - values[1]) < 3 && Math.abs(values[0] - values[2]) < 3 && Math.abs(values[1] - values[2]) < 3)
-        )
+        if (values[0] % 2 == 0 || values[1] % 2 == 0 || values[2] % 2 == 0)
+        {
+            if (values[0] % 2 == 0 && values[1] % 2 == 0 && values[2] % 2 == 0)
+            {
+                balance += 75;
+                showPrize(75);
+            }
+        }
+        else if (Math.abs(values[0] - values[1]) < 3 && Math.abs(values[0] - values[2]) < 3 && Math.abs(values[1] - values[2]) < 3)
         {
             balance += 75;
+            showPrize(75);
         }
     }
     else if ((values[0] > 6 || values[0] < 4) && (values[1] > 6 || values[1] < 4) && (values[2] > 6 || values[2] < 4))
     {
         //Checking combination of BARs
 
-        if (values[0] % 2 == 0 && values[1] % 2 == 0 && values[2] % 2 == 0)
+        if (values[0] % 2 == 0 || values[1] % 2 == 0 || values[2] % 2 == 0)
         {
-            balance += 5;
+            if (values[0] % 2 == 0 && values[1] % 2 == 0 && values[2] % 2 == 0)
+            {
+                balance += 5;
+                showPrize(5);
+            }
         }
         else
         {
@@ -266,6 +312,7 @@ function stopSpin ()
             if (Math.abs(values[0] - values[1]) < 6 && Math.abs(values[0] - values[2]) < 6 && Math.abs(values[1] - values[2]) < 6)
             {
                 balance += 5;
+                showPrize(5);
             }
         }
     }
